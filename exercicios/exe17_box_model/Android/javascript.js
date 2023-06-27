06b4b583d503f11adf2d2a50d3731a89ad4614c4
Jaimepelembe <105989003+Jaimepelembe@users.noreply.github.com>
2023-06-27 21:09:02 +0200

    Conclui a criacao do site do android
    Tornei o video responsivo;
    Consertei o bug do header;

--- exercicios/exe17_box_model/Android/javascript.js
+++ exercicios/exe17_box_model/Android/javascript.js
@@ -4,43 +4,35 @@ var content = document.getElementById("article_main");
 var showSidebar = false;
 
 function toggleSidebar() {
-  showSidebar = !showSidebar; /*Chane the value of showSidevar*/
+  showSidebar = !showSidebar; /*Change the value of showSidevar*/
   if (showSidebar) {
     navigation_header.style.marginLeft = "-1vw"; /*Show the sidebar*/
     navigation_header.style.animationName = "showSidebar";
     content.style.filter = "blur(2px)"; /**Desfoca o conteudo principal*/
   } else {
-      navigation_header.style.marginLeft = "-100vw"; /*hide the sidebar*/
+    navigation_header.style.marginLeft = "-100vw"; /*hide the sidebar*/
     navigation_header.style.animationName = "";
     content.style.filter = "";
-    update_gap();
   }
 }
 
 /*Fecha o sidebar clicando no conteudo principal */
-
 function closeSidebar() {
   if (showSidebar) {
     toggleSidebar();
   }
 }
 
-function update_gap() {
-  if (window.innerWidth > 750) {
-    navigation_header.style.gap = "50px";
-    navigation_header.style.fontSize = "20px";
-  
-  } else {
-    navigation_header.style.gap = "20px";
-    navigation_header.style.fontSize = "18px";
-  }
-}
-
 window.addEventListener("resize", function (event) {
-  update_gap();
   /*Fechar o sidebar ao redimensionar a janela*/
-  if (window.innerWidth > 750 && showSidebar) {
-    toggleSidebar();
-
+  if (window.innerWidth > 750) {
+    if (showSidebar) {
+      toggleSidebar();
+    } else {
+      navigation_header.style.marginLeft = "-1vw";
+    }
+  } else {
+    navigation_header.style.marginLeft = "-100vw";
   }
+
 });
