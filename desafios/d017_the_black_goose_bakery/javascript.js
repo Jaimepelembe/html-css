9a4df0d03e66e3a1a6fdd649c22c405389f99d5a
Jaimepelembe <105989003+Jaimepelembe@users.noreply.github.com>
2023-07-31 19:03:31 +0200

    Actualizei o desafio de black Goose
    -Troquei o tamanho da fonte do header;
    -ajustei melhor a parte do aside;

--- desafios/d017_the_black_goose_bakery/javascript.js
+++ desafios/d017_the_black_goose_bakery/javascript.js
@@ -1,6 +1,7 @@
 var header = document.getElementById("header");
 var navigation_header = document.getElementById("navigation_header");
 var conteudo = document.getElementById("main");
+var horario_funcionamento = document.getElementById("horario_funcionamento");
 var showSidebar = false;
 
 function mudar_estado_sidebar() {
@@ -10,10 +11,12 @@ function mudar_estado_sidebar() {
     navigation_header.style.marginLeft = "0vw";
     navigation_header.style.animationName = "showSidebar";
     conteudo.style.filter = "blur(2px)";
+    horario_funcionamento.style.filter = "blur(2px)";
   } else {
     navigation_header.style.marginLeft = "-100vw";
     navigation_header.style.animationName = "";
     conteudo.style.filter = "";
+    horario_funcionamento.style.filter = "";
   }
 }
 
