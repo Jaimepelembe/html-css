ee424685b91679ea2deb9dedf9b34e99662a2cc4
Jaimepelembe <105989003+Jaimepelembe@users.noreply.github.com>
2023-08-17 15:58:22 +0200

    feat:done the css of computer and print verision of Page The  Black Goose Bakery.

--- desafios/d017_the_black_goose_bakery/javascript.js
+++ desafios/d017_the_black_goose_bakery/javascript.js
@@ -1,40 +1,49 @@
 var header = document.getElementById("header");
 var navigation_header = document.getElementById("navigation_header");
-var conteudo = document.getElementById("main");
-var horario_funcionamento = document.getElementById("horario_funcionamento");
-var showSidebar = false;
+var content = document.getElementById("main");
+var schedule_operating = document.getElementById("schedule_operating");
+var showingSidebar = false;
 
-function mudar_estado_sidebar() {
-  showSidebar = !showSidebar;
+function changeStatusSidebar() {
+  showingSidebar = !showingSidebar;
 
-  if (showSidebar) {
-    navigation_header.style.marginLeft = "0vw";
-    navigation_header.style.animationName = "showSidebar";
-    conteudo.style.filter = "blur(2px)";
-    horario_funcionamento.style.filter = "blur(2px)";
+  if (showingSidebar) {
+    showSidebar();
   } else {
-    navigation_header.style.marginLeft = "-100vw";
-    navigation_header.style.animationName = "";
-    conteudo.style.filter = "";
-    horario_funcionamento.style.filter = "";
+    closeSidebar();
   }
 }
 
+function showSidebar() {
+  navigation_header.style.marginLeft = "0vw";
+  navigation_header.style.animationName = "showSidebar";
+  content.style.filter = "blur(2px)";
+  schedule_operating.style.filter = "blur(2px)";
+}
+
+function closeSidebar() {
+  navigation_header.style.marginLeft = "-100vw";
+  navigation_header.style.animationName = "";
+  content.style.filter = "";
+  schedule_operating.style.filter = "";
+}
+
 /*Fechar o sidebar ao clicar no conteudo principal */
-function fecharSidebar() {
-  if (showSidebar) {
-    mudar_estado_sidebar();
+function endtheSidebar() {
+  if (showingSidebar) {
+    changeStatusSidebar();
   }
 }
 
 function fechar_sidebar_redimensionartela() {
-  if (window.innerWidth > 750) {
+  if (window.innerWidth > 768) {
     navigation_header.style.position =
-      "relative"; /*o nav volta acupar a sua posicao original na tela*/
-    fecharSidebar();
+      "absolute"; /*o nav volta acupar a sua posicao original na tela*/
+    navigation_header.style.marginLeft = "0vw";
   } else {
     navigation_header.style.position =
       "fixed"; /*O nav ocupa um espaco fixo na tela */
+    closeSidebar();
   }
 }
 
